118320b50390f67e18d8e27e1051e1427164ac50
Anthony Federico <anfederico@users.noreply.github.com>
2019-03-19 13:46:18 -0400

    Testing bot
    #20

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='Gemini',
-    version='0.1.0',
+    version='1.0.0',
     description='Backtesting for sleepless cryptocurrency markets',
     long_description=readme,
     url='https://github.com/anfederico/Gemini',
